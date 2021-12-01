22072f1d748ead9253bb9731cb4902a86e688ba0
Jason Rietzke <jason.rietzke@livereader.com>
2021-12-01 14:15:03 +0100

    solution day01 puzzle01

---
+++ Dec.01/puzzle_01.py
@@ -0,0 +1,15 @@
+
+
+input = open("input_01.txt")
+lines = [int(line.strip()) for line in input]
+input.close()
+
+increase_count = 0
+prev_value = 0
+for value in lines:
+	if prev_value > 0 and value > prev_value:
+		increase_count += 1
+	prev_value = value
+
+print(increase_count)
+
