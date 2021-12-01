86202329b4d7bc527e222c06487589f9d37c4098
Jason Rietzke <jason.rietzke@livereader.com>
2021-12-01 20:50:35 +0100

    solution day02 puzzle02

--- Dec.01/puzzle_01.py
+++ Dec.01/puzzle_01.py
@@ -6,6 +6,7 @@ input.close()
 
 increase_count = 0
 prev_value = 0
+
 for value in lines:
 	if prev_value > 0 and value > prev_value:
 		increase_count += 1
---
+++ Dec.01/puzzle_02.py
@@ -0,0 +1,19 @@
+
+
+input = open("input_01.txt")
+lines = [int(line.strip()) for line in input]
+input.close()
+
+increase_count = 0
+prev_sum = 0
+
+for i in range(len(lines)):
+	sum = lines[i]
+	if prev_sum > 0 and len(lines) > i + 2:
+		sum = lines[i] + lines[i + 1] + lines[i + 2]
+		if sum > prev_sum:
+			increase_count += 1
+	prev_sum = sum
+
+print(increase_count)
+
