bf1d3676ab8bda429f0e3ead6ff2873178a3c884
Jason Rietzke <jason.rietzke@livereader.com>
2021-12-14 11:27:00 +0100

    solution day14 puzzle02

--- Dec.14/puzzle_01_02.py
+++ Dec.14/puzzle_01_02.py
@@ -5,22 +5,20 @@ input_lines = [line.strip() for line in input]
 input.close()
 
 polymer = input_lines[0]
-pair_insertion_rules = []
+pair_insertion_rules = {}
 for line in input_lines[2:]:
-	pair_insertion_rules.append({
-		"template": line.split(" -> ")[0],
-		"insertion": line.split(" -> ")[1]
-	})
+	pair_insertion_rules[line.split(" -> ")[0]] = line.split(" -> ")[1]
 
-iterations = 10
+iterations = 40 # 10 for Part 1 and 40 for Part 2
 
 for i in range(iterations):
-	for j in reversed(range(len(polymer) - 1)):
-		pair = polymer[j] + polymer[j + 1]
-		for rule in pair_insertion_rules:
-			if pair == rule["template"]:
-				polymer = polymer[:j + 1] + rule["insertion"] + polymer[j + 1:]
-				break
+	print("Iteration: {}" .format(i + 1))
+	print("Polymer length: {:,}" .format(len(polymer)))
+	j = 0
+	while j < len(polymer) - 1:
+		pair = polymer[j:j+2]
+		polymer = polymer[:j + 1] + pair_insertion_rules[pair] + polymer[j + 1:]
+		j += 2
 
 element_counts = {}
 for element in polymer:
@@ -28,8 +26,6 @@ for element in polymer:
 		element_counts[element] += 1
 	else:
 		element_counts[element] = 1
-
-print(element_counts)
 
 most_common_element = {
 	"element": "",
@@ -48,3 +44,4 @@ for element in element_counts:
 		least_common_element["count"] = element_counts[element]
 
 print("The most common element subtracted with the least common element is: {}" .format(most_common_element["count"] - least_common_element["count"]))
+
