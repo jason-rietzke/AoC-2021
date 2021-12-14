ea3b16651d5d5aad5ddcf8c47298e968b27f1d8a
Jason Rietzke <jason.rietzke@livereader.com>
2021-12-14 21:19:18 +0100

    update algorithm day14

--- Dec.14/puzzle_01_02.py
+++ Dec.14/puzzle_01_02.py
@@ -9,23 +9,57 @@ pair_insertion_rules = {}
 for line in input_lines[2:]:
 	pair_insertion_rules[line.split(" -> ")[0]] = line.split(" -> ")[1]
 
-iterations = 40 # 10 for Part 1 and 40 for Part 2
+total_depth = 20 # 10 for Part 1 and 40 for Part 2
+elements = {}
 
-for i in range(iterations):
-	print("Iteration: {}" .format(i + 1))
-	print("Polymer length: {:,}" .format(len(polymer)))
-	j = 0
-	while j < len(polymer) - 1:
-		pair = polymer[j:j+2]
-		polymer = polymer[:j + 1] + pair_insertion_rules[pair] + polymer[j + 1:]
-		j += 2
+print("{} calculations needed" .format(pow(2, total_depth)))
 
-element_counts = {}
+class Node:
+
+	def __init__(self, pair):
+		self.pair = pair
+		self.parent = None
+		self.depth = None
+	
+	def run(self):
+		if self.depth >= total_depth:
+			return
+
+		element = pair_insertion_rules[self.pair]
+		if element in elements:
+			elements[element] += 1
+		else:
+			elements[element] = 1
+
+		pair_1 = self.pair[0] + element
+		child_1 = Node(pair_1)
+		child_1.depth = self.depth + 1
+		child_1.parent = self
+		child_1.run()
+
+		pair_2 = element + self.pair[1]
+		child_2 = Node(pair_2)
+		child_2.depth = self.depth + 1
+		child_2.parent = self
+		child_2.run()
+
+
 for element in polymer:
-	if element in element_counts:
-		element_counts[element] += 1
+	if element in elements:
+			elements[element] += 1
 	else:
-		element_counts[element] = 1
+		elements[element] = 1
+
+length = len(polymer)
+for i in range(length - 1):
+	pair = polymer[i:i+2]
+	sub_path = 0
+	node = Node(pair)
+	node.depth = 0
+	node.run()
+	print("{} \t {}%".format(polymer[i:i+2], round(100 * (i+2) / length, 2)))
+
+print(elements)
 
 most_common_element = {
 	"element": "",
@@ -35,13 +69,12 @@ least_common_element = {
 	"element": "",
 	"count": 0
 }
-for element in element_counts:
-	if element_counts[element] > most_common_element["count"]:
+for element in elements:
+	if elements[element] > most_common_element["count"]:
 		most_common_element["element"] = element
-		most_common_element["count"] = element_counts[element]
-	if element_counts[element] < least_common_element["count"] or least_common_element["count"] == 0:
+		most_common_element["count"] = elements[element]
+	if elements[element] < least_common_element["count"] or least_common_element["count"] == 0:
 		least_common_element["element"] = element
-		least_common_element["count"] = element_counts[element]
+		least_common_element["count"] = elements[element]
 
 print("The most common element subtracted with the least common element is: {}" .format(most_common_element["count"] - least_common_element["count"]))
-
